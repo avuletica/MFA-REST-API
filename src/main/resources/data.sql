0fb09b5226fb60414dfb6296eb6d68271d199061
Ante <vuletic23@gmail.com>
2017-12-09 10:36:50 +0100

    Add initial db data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO app_user (id, first_name, last_name, username, password) VALUES (1, 'Ante', 'Antic', 'ante', '123');
+INSERT INTO app_user (id, first_name, last_name, username, password) VALUES (2, 'Toad', 'Boss', 'toad', '123');
+INSERT INTO app_user (id, first_name, last_name, username, password) VALUES (3, 'Bob', 'King', 'bob', '123');
+INSERT INTO app_user (id, first_name, last_name, username, password) VALUES (4, 'Test', 'Doe', 'test', '123');
+INSERT INTO app_user (id, first_name, last_name, username, password) VALUES (5, 'Tom', 'Moody', 'tom', '123');
+INSERT INTO app_user (id, first_name, last_name, username, password) VALUES (6, 'Henry', 'Fox', 'henry', '123');
