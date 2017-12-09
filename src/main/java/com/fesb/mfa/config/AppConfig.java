af5dab9336b2dbe846fd7cfa17d528dd6c3f94a3
Ante <vuletic23@gmail.com>
2017-12-09 21:16:03 +0100

    Add global CORS mappings

--- src/main/java/com/fesb/mfa/config/AppConfig.java
+++ src/main/java/com/fesb/mfa/config/AppConfig.java
@@ -1,4 +1,17 @@
 package com.fesb.mfa.config;
 
-public class AppConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
 }
