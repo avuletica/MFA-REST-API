60c9728b3b62b36ec70a1c12a2088e026b6fad56
avuletica <vuletic23@gmail.com>
2018-01-07 14:36:25 +0100

    implement new endpoint for user information

--- src/main/java/com/fesb/mfa/user/rest/UserController.java
+++ src/main/java/com/fesb/mfa/user/rest/UserController.java
@@ -3,13 +3,10 @@ package com.fesb.mfa.user.rest;
 import com.fesb.mfa.user.domain.ApplicationUser;
 import com.fesb.mfa.user.domain.ApplicationUserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/security")
 public class UserController {
 
     private ApplicationUserRepository applicationUserRepository;
@@ -25,5 +22,10 @@ public class UserController {
     public void signUp(@RequestBody ApplicationUser user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         applicationUserRepository.save(user);
+    }
+
+    @GetMapping("/user/{userName}")
+    public ApplicationUser getUserInformation(@PathVariable String userName) {
+        return applicationUserRepository.findByUsername(userName);
     }
 }
