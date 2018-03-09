7abbe0da0e237a7f6fd504872a10d2c208a562c4
Ante <vuletic23@gmail.com>
2018-03-09 15:24:17 +0100

    delete unused code

--- src/main/java/com/fesb/mfa/task/domain/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fesb.mfa.task.domain;
-
-import com.fesb.mfa.task.domain.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
